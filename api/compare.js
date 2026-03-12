5607f70d9895fa642a7679d57288a37365141a9b
Cassidy Spencer <100648075+cassidyspen@users.noreply.github.com>
2026-03-11 23:00:06 -0400

    Add Cache-Control header to response

--- api/compare.js
+++ api/compare.js
@@ -1,6 +1,7 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
   if (req.method === 'OPTIONS') return res.status(200).end();
 
   const { type, city_name, country_name } = req.query;
